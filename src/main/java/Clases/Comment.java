6d2db6d5225da1af2ee5a10e68833b758d67b6f1
judajocu <juandaniejoa@gmail.com>
2018-07-19 13:32:29 -0400

    Comment class setup

--- src/main/java/Clases/Comment.java
+++ src/main/java/Clases/Comment.java
@@ -19,6 +19,62 @@ public class Comment implements Serializable{
     @ManyToOne()
     private image image;
 
+    public Comment(User usuario, String text, List<Tag> tags, Post post, Clases.image image) {
+        this.usuario = usuario;
+        Text = text;
+        this.tags = tags;
+        this.post = post;
+        this.image = image;
+    }
 
+    public Comment() {
+    }
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public User getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(User usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getText() {
+        return Text;
+    }
+
+    public void setText(String text) {
+        Text = text;
+    }
+
+    public List<Tag> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<Tag> tags) {
+        this.tags = tags;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+    public Clases.image getImage() {
+        return image;
+    }
+
+    public void setImage(Clases.image image) {
+        this.image = image;
+    }
 }
